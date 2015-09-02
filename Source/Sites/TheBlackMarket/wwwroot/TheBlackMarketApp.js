96c5584d27399ed82611c8d96a82987b5f51ca17
rpowers3 <rpowers3@gmail.com>
2015-09-02 11:36:07 -0400

    Adding in analytics.

--- Source/Sites/TheBlackMarket/wwwroot/TheBlackMarketApp.js
+++ Source/Sites/TheBlackMarket/wwwroot/TheBlackMarketApp.js
@@ -35,6 +35,12 @@
 			});
 	}]);
 
+	TheBlackMarketAppModule.run(['$rootScope', '$window', '$location', function($rootScope, $window, $location) {
+		$rootScope.$on('$routeChangeSuccess', function() {
+			$window.ga('send', 'pageview', { page: $location.url() });
+		});
+	}]);
+
 	TheBlackMarketAppModule.filter('percentage', ['$filter', function($filter) {
 		return function(input, decimals) {
 			return $filter('number')(input * 100, decimals) + '%';
